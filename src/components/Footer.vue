89c724d83df564f819bfe13e6b5abd0fd8032ff6
Askar Zhumabekov <askar.zhumabekovv@gmail.com>
2020-11-26 21:52:57 +0600

    Поправки 'Инф. стр'

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,168 +1,185 @@
 <template>
   <footer>
     <div class="wrapper">
-      <div class="loading-bar" v-if="(totalpercent != 100) && statData">
-        <div class="percentage" :style="{'width' : percentage + '%'}"><span v-show="percentage>0">{{percent}} %</span></div>
+      <div class="loading-bar" v-if="totalpercent != 100 && statData">
+        <div class="percentage" :style="{ width: percentage + '%' }">
+          <span v-show="percentage > 0">{{ percent }} %</span>
+        </div>
       </div>
       <div v-else-if="statData">
-        <button class="btn_certificate" @click="getCertificate()">Получть сертификат</button>
+        <button class="btn_certificate" @click="getCertificate()">
+          Получть сертификат
+        </button>
       </div>
       <div v-else></div>
-      <div class="bottom_text">CAAPL, 2020</div>
+      <div class="bottom_text">CAAPL 2020</div>
     </div>
   </footer>
 </template>
 
 <script>
-import {mapActions, mapMutations} from 'vuex'
+import { mapActions, mapMutations } from "vuex";
 export default {
-  name: 'Footer',
+  name: "Footer",
   data: () => {
     return {
       totalpercent: 0,
       percentage: 0,
       statData: false,
-      certificate: ""
-    }
+      certificate: "",
+    };
   },
   computed: {
     percent() {
       return this.percentage.toFixed();
-    }
+    },
   },
   watch: {
-    totalpercent: function () {
+    totalpercent: function() {
       this.animationProgress();
     },
-    '$route'(){
+    $route() {
       this.initVar();
       this.clearLectionData();
-    }
+    },
   },
   methods: {
-    ...mapActions(['getTotalStat', 'getCertificateData']),
-    ...mapMutations(['clearLectionData']),
+    ...mapActions(["getTotalStat", "getCertificateData"]),
+    ...mapMutations(["clearLectionData"]),
     animationProgress() {
       var intval = setInterval(() => {
-        if(this.percentage < this.totalpercent)
-          this.percentage += .5;
-        else
-          clearInterval(intval);
+        if (this.percentage < this.totalpercent) this.percentage += 0.5;
+        else clearInterval(intval);
       }, 10);
     },
     initVar() {
-      this.getTotalStat()
-      .then((resp)=>{
-        if(resp.data){
-          if (localStorage.getItem('userAttemptId')){
+      this.getTotalStat().then((resp) => {
+        if (resp.data) {
+          if (localStorage.getItem("userAttemptId")) {
             this.statData = true;
-          }else{
+          } else {
             this.statData = false;
           }
-          this.totalpercent = (100/resp.data.total)*resp.data.passed;
+          this.totalpercent = (100 / resp.data.total) * resp.data.passed;
         }
-      })
+      });
     },
     getCertificate() {
-      this.getCertificateData(localStorage.getItem('userAttemptId'))
-      .then((resp)=>{
-        if(resp.data){
-         console.log(resp.data);
-          let date = new Date().toISOString();
-          let blob = new Blob([resp.data], {type: 'application/pdf'});
-          let downloadUrl = URL.createObjectURL(blob);
-          let link = document.createElement('a');
-          link.href = downloadUrl;
-          link.download = date + '_sertificate.pdf';
-          link.click();
-        }
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
-    }
+      this.getCertificateData(localStorage.getItem("userAttemptId"))
+        .then((resp) => {
+          if (resp.data) {
+            console.log(resp.data);
+            let date = new Date().toISOString();
+            let blob = new Blob([resp.data], { type: "application/pdf" });
+            let downloadUrl = URL.createObjectURL(blob);
+            let link = document.createElement("a");
+            link.href = downloadUrl;
+            link.download = date + "_sertificate.pdf";
+            link.click();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   created() {
     this.animationProgress();
   },
   mounted() {
-    this.initVar()
-  }
-}
+    this.initVar();
+  },
+};
 </script>
 
 <style scoped>
-  footer{
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    background-color: #4c4b4b;
+footer {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #4c4b4b;
+}
+.wrapper {
+  width: 90%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.bottom_text {
+  color: #fff;
+  justify-self: end;
+}
+.loading-bar {
+  position: relative;
+  width: 40%;
+  height: 30px;
+  border-radius: 15px;
+  overflow: hidden;
+  border-bottom: 1px solid #898989;
+  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), 0 -1px 1px #898989,
+    0 1px 0 #898989;
+}
+.loading-bar .percentage {
+  position: absolute;
+  top: 1px;
+  left: 1px;
+  right: 1px;
+  display: block;
+  text-align: center;
+  line-height: 30px;
+  font-size: 0.8em;
+  height: 100%;
+  color: #fff;
+  border-radius: 15px;
+  background-color: #00a189;
+  background-size: 30px 30px;
+  background-image: linear-gradient(
+    135deg,
+    rgba(255, 255, 255, 0.15) 25%,
+    transparent 25%,
+    transparent 50%,
+    rgba(255, 255, 255, 0.15) 50%,
+    rgba(255, 255, 255, 0.15) 75%,
+    transparent 75%,
+    transparent
+  );
+  animation: animate-stripes 3s linear infinite;
+}
+.btn_certificate {
+  background-color: #00a189;
+  font-size: 0.9em;
+  padding: 0.5em 2em;
+  border-radius: 2px;
+  color: #fff;
+  border: 2px solid #00a189;
+}
+.btn_certificate:hover {
+  background-color: #4c4b4b;
+  color: #fff;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+@keyframes animate-stripes {
+  0% {
+    background-position: 0 0;
   }
-  .wrapper{
-    width: 90%;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  100% {
+    background-position: 60px 0;
   }
-  .bottom_text{
-    color: #fff;
-    justify-self: end;
+}
+@media screen and (max-width: 500px) {
+  .btn_certificate {
+    padding: 0.2em 1em;
   }
-  .loading-bar {
-    position: relative;
-    width: 40%;
-    height: 30px;
-    border-radius: 15px;
-    overflow: hidden;
-    border-bottom: 1px solid #898989;
-    box-shadow: inset 0 1px 2px rgba(0,0,0, .4), 0 -1px 1px #898989, 0 1px 0 #898989;
-  }
-  .loading-bar .percentage {
-    position: absolute;
-    top: 1px; left: 1px; right: 1px;
-    display: block;
-    text-align: center;
-    line-height: 30px;
+}
+@media screen and (max-width: 400px) {
+  .bottom_text {
     font-size: 0.8em;
-    height: 100%;
-    color: #fff;
-    border-radius: 15px;
-    background-color: #00a189;
-    background-size: 30px 30px;
-    background-image: linear-gradient(135deg, rgba(255,255,255, .15) 25%, transparent 25%, transparent 50%, rgba(255,255,255, .15) 50%, rgba(255,255,255, .15) 75%, transparent 75%, transparent); 
-    animation: animate-stripes 3s linear infinite;
-  } 
-  .btn_certificate{
-    background-color: #00a189;
-    font-size: .9em;
-    padding: .5em 2em;
-    border-radius: 2px;
-    color: #fff;
-    border: 2px solid #00a189;
   }
-  .btn_certificate:hover{
-    background-color: #4c4b4b;
-    color: #fff;
-    transition: all .5s;
-    cursor: pointer;
+  .btn_certificate {
+    font-size: 0.8em;
+    padding: 0.2em 0.5em;
   }
-  @keyframes animate-stripes {
-    0% { background-position: 0 0; }
-    100% { background-position: 60px 0; }
-  }
-  @media screen and (max-width:500px){
-    .btn_certificate{
-      padding: .2em 1em;
-    }
-  }
-  @media screen and (max-width:400px){
-    .bottom_text{
-      font-size: .8em;
-    }
-    .btn_certificate{
-      font-size: .8em;
-      padding: .2em .5em;
-    }
-  }
+}
 </style>
--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -1,168 +1,217 @@
 <template>
   <section>
     <div v-if="loaded">
-     <div class="intro">
-      <div class="video">
-        <video class="video-media" src="http://176.119.159.61/frontassets/videobg1.5.mp4" muted autoplay loop></video>
+      <div class="intro">
+        <div class="video">
+          <video
+            class="video-media"
+            src="http://176.119.159.61/frontassets/videobg1.5.mp4"
+            muted
+            autoplay
+            loop
+          ></video>
+        </div>
       </div>
-     </div>
-     <div class="wrapper">
-       <div class="title">
-         <span>Цель курса:</span>
-         изменить отношение медицинских работников к людям, живущим с ВИЧ (ЛЖВ), и представителям групп высокого риска, для предоставления качественных услуг и снижения стигматизации и дискриминации в учреждениях здравоохранения, связанной с ВИЧ
-       </div>
-       <div class="content">
-         <span>Задачи курса:</span>
-         <ol>
-          <li>Предоставить участникам информацию об эпидемиологии, этиологию, патогенез, клиника ВИЧ-инфекции и методах профилактики инфицирования.</li>
-          <li>Помочь участникам пересмотреть собственные мысли, убеждения, отношения и поведение в отношении ЛЖВ и представителей ГНР, и их влияние на профессиональное поведение.</li>
-          <li>Рассмотреть права ЛЖВ / представителей ГНР и медицинских работников и их обязанности, связанные с безопасностью оказания медицинских услуг инфицированным пациентам.</li>
-          <li>Ознакомить участников с основными принципами и процедурой проведения добровольного консультирования и тестирования на ВИЧ пациента.</li>
-          <li>Определить понятие «стигма», «стигматизация», «дискриминация» и «толерантное отношение».</li>
-          <li>Выяснить и проанализировать причины возникновения таких явлений, как стигматизация и дискриминация, и осознать последствия стигматизации ЛЖВ и представителей ГНР.</li>
-          <li>Обсудить с участниками соблюдения универсальных методов предосторожности во избежание инфицирования ВИЧ на рабочем месте.</li>
-          <li>Ознакомить участников тренинга с алгоритмом действий и правилам назначения постконтактной профилактики.</li>
-         </ol>
-       </div>
-       <button class="lecture__test_btn" @click.prevent="familiarized()">{{lectionData.passed == lectionData.total ? "Назад" : "Ознакомиться"}}</button>
-     </div>
+      <div class="wrapper">
+        <div class="title">
+          <span>Цель курса:</span>
+          изменить отношение медицинских работников к людям, живущим с ВИЧ
+          (ЛЖВ), и представителям групп высокого риска, для предоставления
+          качественных услуг и снижения стигматизации и дискриминации в
+          учреждениях здравоохранения, связанной с ВИЧ
+        </div>
+        <div class="content">
+          <span>Задачи курса:</span>
+          <ol>
+            <li>
+              Обучающий курс предназначен для медицинского персонала всех
+              уровней(врачи, медицинские сестры, психологи) по теме:
+              формирование толерантного отношения со стороны медицинских
+              сотрудников к людям, которые живут с ВИЧ и представителям ключевых
+              групп.
+            </li>
+            <li>
+              Предоставить участникам информацию об эпидемиологии, этиологию,
+              патогенез, клиника ВИЧ-инфекции и методах профилактики
+              инфицирования.
+            </li>
+            <li>
+              Помочь участникам пересмотреть собственные мысли, убеждения,
+              отношения и поведение в отношении ЛЖВ и представителей ключевых
+              групп, и их влияние на профессиональное поведение.
+            </li>
+            <li>
+              Рассмотреть права ЛЖВ / представителей ключевых групп и
+              медицинских работников и их обязанности, связанные с безопасностью
+              оказания медицинских услуг инфицированным пациентам.
+            </li>
+            <li>
+              Ознакомить участников с основными принципами и процедурой
+              проведения добровольного консультирования и тестирования на ВИЧ
+              пациента.
+            </li>
+            <li>
+              Определить понятие «стигма», «стигматизация», «дискриминация» и
+              «толерантное отношение».
+            </li>
+            <li>
+              Выяснить и проанализировать причины возникновения таких явлений,
+              как стигматизация и дискриминация, и осознать последствия
+              стигматизации ЛЖВ и представителей ключевых групп.
+            </li>
+            <li>
+              Обсудить с участниками соблюдения универсальных методов
+              предосторожности во избежание инфицирования ВИЧ на рабочем месте.
+            </li>
+            <li>
+              Ознакомить участников тренинга с алгоритмом действий и правилам
+              назначения постконтактной профилактики.
+            </li>
+          </ol>
+        </div>
+        <button class="lecture__test_btn" @click.prevent="familiarized()">
+          {{
+            lectionData.passed == lectionData.total ? "Назад" : "Ознакомиться"
+          }}
+        </button>
+      </div>
     </div>
     <div class="loadingFon" v-else>Loading...</div>
   </section>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 export default {
-  name: 'InfoCart',
-  props: ['id'],
+  name: "InfoCart",
+  props: ["id"],
   data() {
     return {
-      loaded: false
-    }
+      loaded: false,
+    };
   },
   computed: {
-    ...mapGetters(['lectionData'])
+    ...mapGetters(["lectionData"]),
   },
   methods: {
-    ...mapActions(['getLectionData', 'sendFamiliarize']),
+    ...mapActions(["getLectionData", "sendFamiliarize"]),
     familiarized() {
-      this.sendFamiliarize({lessonStatId: this.lectionData.id})
-      .then((resp)=>{
-        if(resp.data){
-          this.$router.push('/lection')
-        }else{
-          this.$router.push('/lection')
+      this.sendFamiliarize({ lessonStatId: this.lectionData.id }).then(
+        (resp) => {
+          if (resp.data) {
+            this.$router.push("/lection");
+          } else {
+            this.$router.push("/lection");
+          }
         }
-      })
-    }
+      );
+    },
   },
   mounted() {
-    this.getLectionData(this.id)
-      .then((resp)=>{
-        if(resp.data){
-          this.loaded = true;
-        }
-      })
-    if (!localStorage.getItem('userAttemptId')){
-      this.$router.push('/')
+    this.getLectionData(this.id).then((resp) => {
+      if (resp.data) {
+        this.loaded = true;
+      }
+    });
+    if (!localStorage.getItem("userAttemptId")) {
+      this.$router.push("/");
     }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  section{
-    width: 100%;
-    height: calc(100vh - 100px);
-    background-color: #f8f8f8;
-    overflow: auto;
+section {
+  width: 100%;
+  height: calc(100vh - 100px);
+  background-color: #f8f8f8;
+  overflow: auto;
+}
+.loadingFon {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  line-height: 100vh;
+}
+.intro {
+  width: 100%;
+  height: 40vh;
+  background: url("../assets/bg.jpg") center no-repeat;
+  background-size: cover;
+  overflow: hidden;
+  position: relative;
+}
+.intro::after {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+}
+.video {
+  position: relative;
+  z-index: 1;
+}
+.video-media {
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: -5em;
+  left: 0;
+}
+.wrapper {
+  width: 90%;
+  margin: 0 auto;
+  display: grid;
+  padding: 1em;
+  grid-row-gap: 2em;
+  font-size: 0.95em;
+}
+.title span {
+  color: #1769aa;
+  font-weight: bold;
+  font-size: 1.25em;
+}
+.content span {
+  color: #ff9100;
+  font-weight: bold;
+  font-size: 1.25em;
+}
+.lecture__test_btn {
+  align-self: start;
+  justify-self: start;
+  background-color: #00a189;
+  padding: 0.7em 3.5em;
+  margin-top: 1.2em;
+  border-radius: 3px;
+  color: #fff;
+  border: 2px solid #00a189;
+  font-size: 1em;
+}
+.lecture__test_btn:hover {
+  background-color: #f8f8f8;
+  color: #00a189;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+ol {
+  margin-left: 1.2em;
+}
+@media screen and (min-width: 2000px) {
+  .video-media {
+    top: -10em;
   }
-  .loadingFon{
-    display: grid;
-    justify-content: center;
-    align-content: center;
-    line-height: 100vh;
-  }
-  .intro{
-    width: 100%;
-    height: 40vh;
-    background: url("../assets/bg.jpg") center no-repeat;
-    background-size: cover;
-    overflow: hidden;
-    position: relative;
-  }
-  .intro::after{
-    content: "";
-    display: block;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, .7);
-    position: absolute;
+}
+@media screen and (max-width: 1000px) {
+  .video-media {
     top: 0;
-    left: 0;
-    z-index: 2;
   }
-  .video{
-    position: relative;
-    z-index: 1;
+  .intro {
+    height: 30vh;
   }
-  .video-media{
-    width: 100%;
-    height: auto;
-    position: absolute;
-    top: -5em;
-    left: 0;
-  }
-  .wrapper{
-    width: 90%;
-    margin: 0 auto;
-    display: grid;
-    padding: 1em;
-    grid-row-gap: 2em;
-    font-size: .95em;
-  }
-  .title span{
-    color: #1769aa;
-    font-weight: bold;
-    font-size: 1.25em;
-  }
-  .content span{
-    color: #ff9100;
-    font-weight: bold;
-    font-size: 1.25em;
-  }
-  .lecture__test_btn{
-    align-self: start;
-    justify-self: start;
-    background-color: #00a189;
-    padding: .7em 3.5em;
-    margin-top: 1.2em;
-    border-radius: 3px;
-    color: #fff;
-    border: 2px solid #00a189;
-    font-size: 1em;
-  }
-  .lecture__test_btn:hover{
-    background-color: #f8f8f8;
-    color: #00a189;
-    transition: all .5s;
-    cursor: pointer;
-  }
-  ol{
-    margin-left: 1.2em;
-  }
-  @media screen and (min-width:2000px){
-    .video-media{
-      top: -10em;
-    }
-  }
-  @media screen and (max-width:1000px){
-    .video-media{
-      top: 0;
-    }
-    .intro{
-      height: 30vh;
-    }
-  }
+}
 </style>
